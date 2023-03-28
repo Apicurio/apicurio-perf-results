cedd4b427495b0dc7c479022d8fbae4938060bb7
apicurio-ci <apicurio.ci@gmail.com>
2023-03-28 05:40:44 +0000

    Publishing perf-test results for End2EndSimulation run.

---
+++ 2023-03-28/End2EndSimulation/05.40.43-1u-1i/js/all_sessions.js
@@ -0,0 +1,11 @@
+allUsersData = {
+    
+color: '#FF9D00',
+name: 'Active Users',
+data: [
+  [1679982031000,1],[1679982032000,1],[1679982033000,1]
+],
+tooltip: { yDecimals: 0, ySuffix: '', valueDecimals: 0 }
+    , zIndex: 20
+    , yAxis: 1
+};
